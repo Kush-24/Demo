67db1d07410b4c60590ff9b48c50c6df27b27be1
Kush-24 <45205610+Kush-24@users.noreply.github.com>
2019-08-20 15:08:23 +0530

    Add files via upload

---
+++ Java/DataStructure/Queue/QRunner.java
@@ -0,0 +1,21 @@
+package com.wipro.queue;
+
+public class QRunner {
+
+	public static void main(String[] args) {
+		Queue q=new Queue();
+		System.out.println(q.isEmpty());
+		System.out.println(q.peek());
+		q.add(10);
+		q.show();
+		q.add(20);
+		q.show();
+		System.out.println(q.element());
+		System.out.println(q.size());
+
+		q.remove();
+		System.out.println(q.size());
+		System.out.println(q.element());
+	}
+
+}
