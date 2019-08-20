e60e5ed9c5737dfa2a32f9c21d3985d8d30c176f
Kush-24 <45205610+Kush-24@users.noreply.github.com>
2019-08-20 15:21:17 +0530

    Add files via upload

---
+++ Java/DataStructure/New folder/tree/BinaryTree.java
@@ -0,0 +1,24 @@
+package com.wipro.tree;
+
+public class BinaryTree {
+	Node root;
+	
+	public BinaryTree() {
+		root =null;
+	}
+	
+	public BinaryTree(int key) {
+		root=new Node(key);
+	}
+
+	public void show()
+	{
+		Node n=root;
+		while(n!=null)
+		{
+						
+		}
+	}
+	
+	
+}
