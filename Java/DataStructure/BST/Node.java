d1d79240b7d38db79e2db8f59732e997ea7477fb
Kush-24 <45205610+Kush-24@users.noreply.github.com>
2019-08-20 15:05:39 +0530

    BST

---
+++ Java/DataStructure/BST/Node.java
@@ -0,0 +1,12 @@
+package com.wipro.tree;
+
+public class Node {
+		Node left,right;
+		int key;
+		
+		public Node(int item) 
+	    { 
+	        key = item; 
+	        left = right = null; 
+	    } 
+}
